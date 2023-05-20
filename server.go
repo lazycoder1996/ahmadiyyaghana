41fbd83645cf3e50c8389b1982bd1405189720b6
Muhammad Mujtaba Appiah <70382598+lazycoder1996@users.noreply.github.com>
2023-05-20 09:46:36 +0000

    Update server.go

--- server.go
+++ server.go
@@ -18,7 +18,7 @@ func main() {
 	r := gin.Default()
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "3000"
 	}
 	r.LoadHTMLGlob("templates/*.tmpl.html")
 	r.Static("/static", "static")
